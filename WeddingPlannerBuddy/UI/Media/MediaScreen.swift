66c30a9dcebda3c0340f67212b3c2643a5d8391d
Alexia Aldea <alexia.aldea@mready.net>
2024-12-27 23:56:38 +0200

    finished media tab

--- WeddingPlannerBuddy/UI/Media/MediaScreen.swift
+++ WeddingPlannerBuddy/UI/Media/MediaScreen.swift
@@ -9,24 +9,54 @@ import SwiftUI
 
 struct MediaScreen: View {
     @EnvironmentObject private var navigation: Navigation
+    private let mainNavigation = EnvironmentObjects.navigation
     @StateObject private var viewModel = MediaViewModel()
     
     var body: some View {
         VStack(spacing: 0) {
             LeftNavBarView(title: "Media", hasBackButton: false) { }
             
-            switch viewModel.weddingsState {
-            case .loading:
-                VStack {
-                    Spacer()
-                    HStack {
+            if viewModel.user != nil {
+                switch viewModel.weddingsState {
+                case .loading:
+                    VStack {
                         Spacer()
-                        LoaderView()
+                        HStack {
+                            Spacer()
+                            LoaderView()
+                            Spacer()
+                        }
                         Spacer()
                     }
+                case .failure(_):
+                    HStack {
+                        Text("You have to log in in order to access the content of this tab.")
+                            .foregroundStyle(Color.mainBlack)
+                            .font(.poppinsRegular(size: 16))
+                        Spacer()
+                    }.padding(.horizontal, 16)
+                        .padding(.top, 24)
                     Spacer()
+                case .value(let weddings):
+                    ScrollView(showsIndicators: false) {
+                        Text("Select a wedding for which you want to add memories from your gallery.")
+                            .multilineTextAlignment(.leading)
+                            .foregroundStyle(Color.mainBlack)
+                            .font(.poppinsSemiBold(size: 16))
+                            .padding(.horizontal, 16)
+                            .padding(.top, 24)
+                        
+                        VStack(alignment: .leading, spacing: 20) {
+                            ForEach(weddings, id: \.id) { wedding in
+                                WidgetView(title: wedding.name, icon: .icWedding) {
+                                    let vm = WeddingMediaViewModel(wedding: wedding)
+                                    mainNavigation?.push(WeddingMediaScreen(viewModel: vm).asDestination(), animated: true)
+                                }
+                            }
+                        }
+                    }
                 }
-            case .failure(_):
+            } else {
                 HStack {
                     Text("You have to log in in order to access the content of this tab.")
                         .foregroundStyle(Color.mainBlack)
@@ -35,17 +65,6 @@ struct MediaScreen: View {
                 }.padding(.horizontal, 16)
                     .padding(.top, 24)
                 Spacer()
-            case .value(let weddings):
-                ScrollView(showsIndicators: false) {
-                    VStack(alignment: .leading, spacing: 20) {
-                        ForEach(weddings, id: \.id) { wedding in
-                            WidgetView(title: wedding.name, icon: .icWedding) {
-                                let vm = WeddingMediaViewModel(wedding: wedding)
-                                navigation.push(WeddingMediaScreen(viewModel: vm).asDestination(), animated: true)
-                            }
-                        }
-                    }.padding(.top, 24)
-                }
             }
         }.background(Color.mainWhite)
             .frame(maxWidth: .infinity, maxHeight: .infinity)
--- WeddingPlannerBuddy/UI/Media/WeddingMedia/WeddingMediaViewModel.swift
+++ WeddingPlannerBuddy/UI/Media/WeddingMedia/WeddingMediaViewModel.swift
@@ -7,17 +7,42 @@
 
 import Foundation
 import Combine
+import UIKit
+
+enum AddImageCompletion {
+    case added
+    case failed
+}
 
 class WeddingMediaViewModel: BaseViewModel {
+    private let weddingService = WeddingService.shared
     @Published var wedding: Wedding?
+    
+    let eventSubject = PassthroughSubject<AddImageCompletion, Never>()
     
     init(wedding: Wedding) {
         self.wedding = wedding
-        super.init()
-        self.getWeddingMedia()
     }
     
-    func getWeddingMedia() {
-        //TODO
+    func addImage(image: UIImage?) {
+        weddingService.addImage(image: image)
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] completion in
+                guard let self else { return }
+                if case .failure(_) = completion {
+//                    self.wedding?.images.removeAll { $0.id == newTable.id }
+                    self.eventSubject.send(.failed)
+                }
+            } receiveValue: { [weak self] result in
+                guard let self else {return}
+//                if let index = self.wedding?.images.firstIndex(where: { $0.id == newTable.label }) {
+//                    self.tables[index] = result
+//                }
+                if result {
+                    self.eventSubject.send(.added)
+                } else {
+                    self.eventSubject.send(.failed)
+                }
+            }.store(in: &bag)
     }
 }
