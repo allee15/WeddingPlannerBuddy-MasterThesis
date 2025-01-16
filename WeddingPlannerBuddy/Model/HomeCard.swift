359d915816b6e585ccecd0807e7a41a5c6989f0d
Alexia Aldea <alexia.aldea@mready.net>
2025-01-16 17:46:02 +0200

    added home cards

---
+++ WeddingPlannerBuddy/Model/HomeCard.swift
@@ -0,0 +1,16 @@
+//
+//  HomeCard.swift
+//  WeddingPlannerBuddy
+//
+//  Created by Alexia Aldea on 16.01.2025.
+//
+
+import Foundation
+import SwiftUI
+
+struct HomeCard {
+    let id = UUID()
+    let title: String
+    let description: String
+    let image: ImageResource
+}
--- WeddingPlannerBuddy/UI/Home/HomeScreen.swift
+++ WeddingPlannerBuddy/UI/Home/HomeScreen.swift
@@ -12,7 +12,79 @@ struct HomeScreen: View {
     @StateObject private var viewModel = HomeViewModel()
     
     var body: some View {
-        Text("Home screen")
+        VStack(spacing: 0) {
+            LeftNavBarView(title: "Home", hasBackButton: false) { }
+            
+            ScrollView(showsIndicators: false) {
+                VStack(spacing: 20) {
+                    HomeCardView(card: viewModel.weddingCard) {
+                        TabBarCoordinator.instance.tabBarNavigation = .wedding
+                    }
+                    
+                    HomeCardView(card: viewModel.tablesCard) {
+                        TabBarCoordinator.instance.tabBarNavigation = .guests
+                    }
+                    
+                    HomeCardView(card: viewModel.mediaCard) {
+                        TabBarCoordinator.instance.tabBarNavigation = .media
+                    }
+                }.padding(.top, 20)
+                    .padding(.horizontal, 16)
+            }
+        }.background(Color.mainWhite)
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .ignoresSafeArea(.container, edges: [.bottom, .horizontal])
+    }
+    
+    
+}
+
+fileprivate struct HomeCardView: View {
+    let card: HomeCard
+    let action: () -> ()
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 12) {
+            Text(card.title)
+                .font(.poppinsSemiBold(size: 18))
+                .foregroundStyle(Color.mainBlack)
+                .multilineTextAlignment(.leading)
+                .underline()
+            
+            Text(card.description)
+                .font(.poppinsRegular(size: 16))
+                .foregroundStyle(Color.mainBlack)
+                .multilineTextAlignment(.leading)
+            
+            Image(card.image)
+                .resizable()
+                .frame(maxWidth: .infinity)
+                .frame(height: 64)
+            
+            Button {
+                action()
+            } label: {
+                HStack(spacing: 8) {
+                    Text("Check it out")
+                        .font(.poppinsRegular(size: 16))
+                        .foregroundStyle(Color.mainBlack)
+                    
+                    Image(.icItemresultArrow)
+                        .resizable()
+                        .renderingMode(.template)
+                        .frame(width: 16, height: 16)
+                        .foregroundStyle(Color.mainBlack)
+                }
+            }
+        }.padding(.all, 20)
+            .borderWithShadow(borderColor: Color.mainBlack,
+                              width: 2,
+                              cornerRadius: 8,
+                              fillColor: Color.clear,
+                              shadowColor: Color.mainBlack.opacity(0.3),
+                              shadowRadius: 8,
+                              x: -3,
+                              y: 0)
     }
 }
 
