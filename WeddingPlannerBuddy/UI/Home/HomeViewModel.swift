5856ecb3eeb35b54439788e1ecaf8b5bdf017554
Alexia Aldea <alexia.aldea@mready.net>
2025-01-16 17:46:02 +0200

    added home cards

--- WeddingPlannerBuddy/UI/Home/HomeViewModel.swift
+++ WeddingPlannerBuddy/UI/Home/HomeViewModel.swift
@@ -8,5 +8,13 @@
 import Foundation
 
 class HomeViewModel: BaseViewModel {
-    
+    let weddingCard: HomeCard = HomeCard(title: "Start your wedding plans!",
+                                         description: "We provide a wide range of tools to help you plan your dream wedding",
+                                         image: .icTerms)
+    let tablesCard: HomeCard = HomeCard(title: "Create the party's schema",
+                                         description: "Find the best seat for every person that will attend your wedding",
+                                         image: .icTerms)
+    let mediaCard: HomeCard = HomeCard(title: "Keep all memories in one place",
+                                       description: "You and your guests can add photos made during the wedding in the same place",
+                                       image: .icTerms)
 }
