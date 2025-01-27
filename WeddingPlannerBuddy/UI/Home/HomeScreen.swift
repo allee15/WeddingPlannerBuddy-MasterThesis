39ebaa4513bca0ec0971e9c8a13947a5de2a2eb3
Alexia Aldea <alexia.aldea@mready.net>
2025-01-27 16:52:02 +0200

    da

--- WeddingPlannerBuddy/UI/Home/HomeScreen.swift
+++ WeddingPlannerBuddy/UI/Home/HomeScreen.swift
@@ -11,12 +11,134 @@ struct HomeScreen: View {
     @EnvironmentObject private var navigation: Navigation
     @StateObject private var viewModel = HomeViewModel()
     
+    @State var showDatePicker: Bool = false
+    @State var showStartDate: Bool = true
+    @State var showEndDate: Bool = false
+    
     var body: some View {
         VStack(spacing: 0) {
             LeftNavBarView(title: "Home", hasBackButton: false) { }
             
             ScrollView(showsIndicators: false) {
                 VStack(spacing: 20) {
+                    HStack {
+                        Text("Select a day or a period of time in order to get recommendations for weather.")
+                            .foregroundStyle(Color.mainBlack)
+                            .font(.poppinsRegular(size: 16))
+                            .multilineTextAlignment(.leading)
+                        
+                        Spacer()
+                    }
+                    
+                    HStack(spacing: 8) {
+                        PeriodOfTimeFilterButtonView(text: "One day",
+                                                    dateType: .singleDate,
+                                                    selectedDateType: viewModel.selectedDateType) {
+                            if viewModel.selectedDateType != .singleDate {
+                                viewModel.selectedDateType = .singleDate
+                            } else {
+                                viewModel.selectedDateType = .anytime
+                            }
+                        }
+                        
+                        PeriodOfTimeFilterButtonView(text: "Period of time",
+                                                    dateType: .periodOfTime,
+                                                    selectedDateType: viewModel.selectedDateType) {
+                            if viewModel.selectedDateType != .periodOfTime {
+                                viewModel.selectedDateType = .periodOfTime
+                            } else {
+                                viewModel.selectedDateType = .anytime
+                            }
+                        }
+                        
+                        Spacer()
+                    }
+                    
+                    if viewModel.selectedDateType == .singleDate {
+                        VStack(spacing: 0) {
+                            DateView(
+                                placeHolder: "Select date",
+                                date: $viewModel.currentDate,
+                                singleDateSelected: viewModel.currentDate) {
+                                    showDatePicker = true
+                                }.background(Color(hex: "#F7F7F8"))
+                                .cornerRadius(8, corners: [.topLeft, .topRight])
+                                .padding(.top, 16)
+                            
+                            DividerView()
+                            
+                            DatePicker("", selection: $viewModel.currentDate,
+                                       in: (Date())..., displayedComponents: .date)
+                                .datePickerStyle(WheelDatePickerStyle())
+                                .accentColor(Color.mainPink)
+                                .background(Color(hex: "#F7F7F8"))
+                                .cornerRadius(8, corners: [.bottomLeft, .bottomRight])
+                        }
+                        
+                        MainButtonView(text: "Get recommendations") {
+                            viewModel.getRecommendations()
+                        }
+                    }
+                    
+                    
+                    if viewModel.selectedDateType == .periodOfTime {
+                        VStack(spacing: 0) {
+                            DateView(
+                                placeHolder: "Start date",
+                                date: $viewModel.startDate,
+                                selectedStartDate: viewModel.startDate) {
+                                    withAnimation {
+                                        showStartDate = true
+                                        showEndDate = false
+                                    }
+                                }
+                                .background(Color(hex: "#F7F7F8"))
+                                .cornerRadius(8, corners: [.topLeft, .topRight])
+                            
+                            if showStartDate == true {
+                                DividerView()
+                                
+                                DatePicker("", selection: $viewModel.startDate, in: (Date())..., displayedComponents: .date)
+                                    .datePickerStyle(WheelDatePickerStyle())
+                                    .accentColor(Color.mainPink)
+                                    .background(Color(hex: "#F7F7F8"))
+                                    .transition(.opacity)
+                            }
+                            
+                            DividerView()
+                            
+                            DateView(
+                                placeHolder: "End date",
+                                date: $viewModel.endDate,
+                                selectedEndDate: viewModel.endDate) {
+                                    withAnimation {
+                                        showEndDate = true
+                                        showStartDate = false
+                                    }
+                                }
+                                .background(Color(hex: "#F7F7F8"))
+                                .cornerRadius(8, corners: showStartDate == true ? [.bottomLeft, .bottomRight] : [])
+                            
+                            if showEndDate == true {
+                                DividerView()
+                                
+                                DatePicker("", selection: $viewModel.endDate, in: viewModel.startDate..., displayedComponents: .date)
+                                    .datePickerStyle(WheelDatePickerStyle())
+                                    .accentColor(Color.mainPink)
+                                    .background(Color(hex: "#F7F7F8"))
+                                    .cornerRadius(8, corners: [.bottomLeft, .bottomRight])
+                                    .transition(.opacity)
+                            }
+                        }
+                        .background(Color(hex: "#F7F7F8"))
+                        .cornerRadius(8, corners: .allCorners)
+                        .padding(.top, 16)
+                        
+                        MainButtonView(text: "Get recommendations") {
+                            viewModel.getRecommendations()
+                        }
+                    }
+                    
                     HomeCardView(card: viewModel.weddingCard) {
                         TabBarCoordinator.instance.tabBarNavigation = .wedding
                     }
@@ -85,6 +207,69 @@ fileprivate struct HomeCardView: View {
                               shadowRadius: 8,
                               x: -3,
                               y: 0)
+    }
+}
+
+fileprivate struct PeriodOfTimeFilterButtonView: View {
+    let text: String
+    let dateType: TypeOfDate
+    let selectedDateType: TypeOfDate
+    let onButtonTappedHandler: () -> ()
+    
+    var body: some View {
+        Button {
+            onButtonTappedHandler()
+        } label: {
+            Text(text)
+                .font(.poppinsRegular(size: 16))
+                .foregroundStyle(Color.mainBlack)
+                .padding(.vertical, 10)
+                .padding(.horizontal, 12)
+                .background(Color.gray.opacity(0.7))
+                .cornerRadius(8, corners: .allCorners)
+                .border(dateType == selectedDateType ? Color.mainPink : Color.gray.opacity(0.7),
+                        width: 1,
+                        cornerRadius: 8)
+        }
+    }
+}
+
+fileprivate struct DateView: View {
+    let placeHolder: String
+    @Binding var date: Date
+    var selectedStartDate: Date?
+    var selectedEndDate: Date?
+    var singleDateSelected: Date?
+    let onButtonTappedHandler: () -> ()
+    
+    var body: some View {
+        Button {
+            onButtonTappedHandler()
+        } label: {
+            HStack(spacing: 0) {
+                Text(placeHolder)
+                    .font(.poppinsRegular(size: 16))
+                    .foregroundStyle(Color.mainBlack)
+                
+                Spacer()
+                
+                if let selectedStartDate {
+                    Text(date.description)//.medicalFileDateToString())
+                        .font(.poppinsBold(size: 14))
+                        .foregroundStyle(Color.mainPink)
+                } else if let singleDateSelected {
+                    Text(date.description)//.medicalFileDateToString())
+                        .font(.poppinsBold(size: 14))
+                        .foregroundStyle(Color.mainPink)
+                } else if let selectedEndDate {
+                    Text(date.description)//.medicalFileDateToString())
+                        .font(.poppinsBold(size: 14))
+                        .foregroundStyle(Color.mainPink)
+                }
+            }.frame(maxWidth: .infinity)
+                .padding(.vertical, 16)
+                .padding(.horizontal, 16)
+        }
     }
 }
 
--- WeddingPlannerBuddy/UI/Home/HomeViewModel.swift
+++ WeddingPlannerBuddy/UI/Home/HomeViewModel.swift
@@ -7,7 +7,21 @@
 
 import Foundation
 
+enum TypeOfDate {
+    case anytime
+    case singleDate
+    case periodOfTime
+}
+
 class HomeViewModel: BaseViewModel {
+    @Published var selectedDateType: TypeOfDate = .anytime
+    @Published var selectedDate: Date?
+    @Published var selectedStartDate: Date?
+    @Published var selectedEndDate: Date?
+    @Published var currentDate = Date()
+    @Published var startDate = Calendar.current.date(byAdding: .day, value: -1, to: Date())!
+    @Published var endDate = Date()
+    
     let weddingCard: HomeCard = HomeCard(title: "Start your wedding plans!",
                                          description: "We provide a wide range of tools to help you plan your dream wedding",
                                          image: .icTerms)
@@ -17,4 +31,16 @@ class HomeViewModel: BaseViewModel {
     let mediaCard: HomeCard = HomeCard(title: "Keep all memories in one place",
                                        description: "You and your guests can add photos made during the wedding in the same place",
                                        image: .icTerms)
+    
+    func getRecommendations() {
+        switch selectedDateType {
+        case .anytime:
+            break
+            
+        case .singleDate:
+            break //TODO
+        case .periodOfTime:
+            break //TODO
+        }
+    }
 }
