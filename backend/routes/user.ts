2bdb8090a6cbcbb8dc34d1f1e1c9ab64164e11b3
Alexia Aldea <94163344+allee15@users.noreply.github.com>
2025-02-01 21:48:45 +0200

    .

--- backend/routes/user.ts
+++ backend/routes/user.ts
@@ -5,6 +5,6 @@ import * as UserController from '../controllers/userController';
 const filePathname = "/" + path.basename(__filename, ".ts")
 const router = Express.Router();
 
-router.get(filePathname, UserController.addUser);
+router.get("/api/user", UserController.addUser);
 
 export default router;
