357bbe1d1237e1b71ed4dd015254cb8d3d26ae1e
Alexia Aldea <alexia.elena.aldea@gmail.com>
2026-02-21 20:24:58 +0200

    no message

--- backend/predict.py
+++ backend/predict.py
@@ -2,6 +2,6 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.get('/')
-async def index():
-    return {"status": "ok"}
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
