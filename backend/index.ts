16d234c8bfee827ef19cf05384c23f71e3b40076
Alexia Aldea <alexia.elena.aldea@gmail.com>
2026-07-01 16:38:08 +0300

    force rebuild without json requirement

--- backend/index.ts
+++ backend/index.ts
@@ -21,6 +21,8 @@ admin.initializeApp({
   databaseURL: "https://weddingplannerbuddy.firebaseio.com"
 });
 
+
+
 app.use(cors())
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
